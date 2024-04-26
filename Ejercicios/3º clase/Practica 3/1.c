b67b6dcea71e8c69bf324b271fb727c5084e7418
Sebastian Giannini <sebastiangiannini95@gmail.com>
2024-04-26 14:49:31 -0300

    Practica 3 - Ej 1 Completo - CORREGIDO

--- Ejercicios/3º clase/Practica 3/1.c
+++ Ejercicios/3º clase/Practica 3/1.c
@@ -1,6 +1,9 @@
 /*1 . Se ingresa un texto de varias líneas  desde teclado y se guarda en un archivo de texto .
 Luego se muestra la cantidad de palabras que tiene cada línea*/
 
+//agregar menu para contar palabras o escribir texto
+//agregar opcion para salir del programa
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -9,12 +12,30 @@ void escribirTexto(FILE *elArchivo);
 void leerArchivo(FILE *elArchivo);
 
 int main(){
+    int opcion;
     FILE *elArchivo;
-    elArchivo = fopen("texto.txt", "w+");
+    elArchivo = fopen("texto.txt", "a+");
     if (elArchivo != NULL){
-        escribirTexto(elArchivo);
-        rewind(elArchivo);
-        leerArchivo(elArchivo);
+        do
+        {
+            printf("1. Ingresar texto\n2. Contar palabras\n3. Salir\n");
+            scanf("%d", &opcion);
+            switch (opcion)
+            {
+                case 1:
+                    escribirTexto(elArchivo);
+                    break;
+                case 2:
+                    rewind(elArchivo);
+                    leerArchivo(elArchivo);
+                    break;
+                case 3:
+                    break;
+                default:
+                    printf("Opcion no valida\n");
+            }
+        } while (opcion != 3); 
+        
         fclose(elArchivo);
     }
     else{
@@ -25,8 +46,9 @@ int main(){
 void escribirTexto(FILE *elArchivo){
     char texto[100];
     printf("Ingrese el texto a guardar en el archivo: ");
+    fflush(stdin);
     gets(texto);
-    fprintf(elArchivo, "%s", texto);
+    fprintf(elArchivo, "%s\n", texto);
 }
 
 void leerArchivo(FILE *elArchivo){
@@ -35,7 +57,7 @@ void leerArchivo(FILE *elArchivo){
     //recorrer caracter a caracter y contar las palabras
     while(!feof(elArchivo)){
         c = fgetc(elArchivo);
-        if(c == ' ' || c == EOF || c == '\n'){//si el caracter es un espacio, fin de archivo o salto de linea
+        if(c == ' ' || c == '\n'){//si el caracter es un espacio, fin de archivo o salto de linea
             palabras++; //se incrementa la cantidad de palabras
         }
     }
